f8f768bb057fc18bdd69fe23d3fa24c8b206d4e6
AnomylusGit <urath.aarush13@gmail.com>
2024-11-30 03:25:12 -0800

    Positive Negative Number

---
+++ main.py
@@ -0,0 +1,8 @@
+number=int(input("enter any number to check"))
+if number >0:
+    print("this is a positive number")
+elif number <0:
+    print("this is a negative number")
+else:
+    print("this is a neutral number")
+
