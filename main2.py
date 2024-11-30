617777136c2e54424c5f8226dc0ef7935918ec75
AnomylusGit <urath.aarush13@gmail.com>
2024-11-30 03:50:13 -0800

    List

---
+++ main2.py
@@ -0,0 +1,12 @@
+lst=["banana" ,"apple", "orange", "pear", "mango" ]
+print("length of list" , len (lst))
+print("length of list" , len (lst))
+print("last element" , lst[-1])
+lst.append("watermelon")
+print("updated list" , lst)
+lst.remove("pear")
+print("updated list" , lst)
+lst.append("pear")
+print("updated list" , lst)
+lst.sort()
+print("updated list" , lst)
